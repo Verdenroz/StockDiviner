564302e82e829994aa17692823d4e35ad46755a8
harvey <tsenh@farmingdale.edu>
2024-02-12 20:46:53 -0500

    Create and implement initial AlphaVantageAPI with deserializers and data classes

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,11 @@
 module org.farmingdale.stockdiviner {
     requires javafx.controls;
     requires javafx.fxml;
+    requires okhttp3;
+    requires com.google.gson;
 
 
     opens org.farmingdale.stockdiviner to javafx.fxml;
     exports org.farmingdale.stockdiviner;
+    exports org.farmingdale.stockdiviner.model;
 }
---
+++ src/main/java/org/farmingdale/stockdiviner/model/ImplAlphaVantageAPI.java
@@ -0,0 +1,53 @@
+package org.farmingdale.stockdiviner.model;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonSyntaxException;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+
+public class ImplAlphaVantageAPI implements AlphaVantageAPI {
+    private static final String ALPHA_VANTAGE_URL = "https://alpha-vantage.p.rapidapi.com";
+    private static final String apiKey = System.getProperty("alphaVantage.apiKey");
+    private final OkHttpClient client;
+
+    public ImplAlphaVantageAPI() {
+        this.client = new OkHttpClient.Builder()
+                .addInterceptor(chain -> {
+                    Request request = chain.request().newBuilder()
+                            .addHeader("x-rapidapi-key", apiKey)
+                            .addHeader("x-rapidapi-host", "alpha-vantage.p.rapidapi.com")
+                            .build();
+
+                    System.out.println(request.url());
+                    return chain.proceed(request);
+                })
+                .build();
+    }
+
+    @Override
+    public StockData getMonthlyTimeSeries(String symbol) throws IOException {
+        String url = ALPHA_VANTAGE_URL + "/query?function=TIME_SERIES_MONTHLY&symbol=" + symbol + "&apikey=" + apiKey;
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+
+            Gson gson = new GsonBuilder()
+                    .registerTypeAdapter(StockData.class, new StockDataDeserializer())
+                    .registerTypeAdapter(StockData.MetaData.class, new MetaDataDeserializer())
+                    .registerTypeAdapter(StockData.MonthlyTimeSeries.class, new MonthlyTimeSeriesDeserializer())
+                    .create();
+
+            return gson.fromJson(response.body().charStream(), StockData.class);
+        } catch (JsonSyntaxException e) {
+            throw new IOException("Error parsing JSON", e);
+        }
+    }
+}
