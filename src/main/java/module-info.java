ed0b812a7940bff560512f326362e824fd286e39
harvey <tsenh@farmingdale.edu>
2024-02-12 20:46:53 -0500

    Create and implement initial AlphaVantageAPI with deserializers and data classes #StockDiviner-3

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,4 +8,5 @@ module org.farmingdale.stockdiviner {
     opens org.farmingdale.stockdiviner to javafx.fxml;
     exports org.farmingdale.stockdiviner;
     exports org.farmingdale.stockdiviner.model.alphavantage;
+    exports org.farmingdale.stockdiviner.model.lunar;
 }
