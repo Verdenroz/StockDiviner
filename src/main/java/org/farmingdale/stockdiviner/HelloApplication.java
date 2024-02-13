089736793c18ecd17d82bf02f44a8135d75c5b11
harvey <tsenh@farmingdale.edu>
2024-02-12 20:46:13 -0500

    Stores apikey in a secret config.properties and hides the file from git

--- src/main/java/org/farmingdale/stockdiviner/HelloApplication.java
+++ src/main/java/org/farmingdale/stockdiviner/HelloApplication.java
@@ -6,6 +6,8 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 public class HelloApplication extends Application {
     @Override
@@ -18,6 +20,21 @@ public class HelloApplication extends Application {
     }
 
     public static void main(String[] args) {
-        launch();
+        // Load properties and set system property
+        Properties properties = new Properties();
+        try (InputStream input = HelloApplication.class.getClassLoader().getResourceAsStream("config.properties")) {
+            if (input == null) {
+                throw new IOException("Unable to find config.properties");
+            }
+            properties.load(input);
+            String apiKey = properties.getProperty("alphaVantage.apiKey");
+            System.setProperty("alphaVantage.apiKey", apiKey);
+            System.out.println("System property alphaVantage.apiKey set to: " + System.getProperty("alphaVantage.apiKey"));
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return;
+        }
+
+        launch(args);
     }
 }
