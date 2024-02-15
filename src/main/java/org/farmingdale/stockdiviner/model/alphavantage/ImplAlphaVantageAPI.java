a0eef29b0d0f84455dff6da3d34036cec9271500
harvey <tsenh@farmingdale.edu>
2024-02-14 19:39:10 -0500

    Create and implements a LunarCalculator that returns a lunar phase based on a date
     * There are four phases - new moon, first quarter, full moon, last quarter
     #StockDiviner-4
    
    (cherry picked from commit d3fbcfd156ae12a38f7eb06c8bb12caade975c05)

--- src/main/java/org/farmingdale/stockdiviner/model/alphavantage/ImplAlphaVantageAPI.java
+++ src/main/java/org/farmingdale/stockdiviner/model/alphavantage/ImplAlphaVantageAPI.java
@@ -27,8 +27,9 @@ public class ImplAlphaVantageAPI implements AlphaVantageAPI {
                 })
                 .build();
     }
+
     @Override
-    public StockData getMonthlyTimeSeries(String symbol) throws IOException {
+    public MonthlyStockData getMonthlyTimeSeries(String symbol) throws IOException {
         String url = ALPHA_VANTAGE_URL + "/query?function=TIME_SERIES_MONTHLY&symbol=" + symbol + "&apikey=" + apiKey;
 
         Request request = new Request.Builder()
@@ -39,11 +40,12 @@ public class ImplAlphaVantageAPI implements AlphaVantageAPI {
             if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
 
             Gson gson = new GsonBuilder()
-                    .registerTypeAdapter(StockData.class, new StockDataDeserializer())
-                    .registerTypeAdapter(StockData.MonthlyTimeSeries.class, new MonthlyTimeSeriesDeserializer())
+                    .registerTypeAdapter(MonthlyStockData.class, new MonthlyStockDataDeserializer())
+                    .registerTypeAdapter(MonthlyStockData.MetaData.class, new MetaDataDeserializer())
+                    .registerTypeAdapter(MonthlyStockData.MonthlyTimeSeries.class, new MonthlyTimeSeriesDeserializer())
                     .create();
 
-            return gson.fromJson(response.body().charStream(), StockData.class);
+            return gson.fromJson(response.body().charStream(), MonthlyStockData.class);
         } catch (JsonSyntaxException e) {
             throw new IOException("Error parsing JSON", e);
         }
