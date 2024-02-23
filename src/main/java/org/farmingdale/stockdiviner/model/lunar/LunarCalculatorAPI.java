35212ad10378e041f6e57cb16d419dc29e1ca540
harvey <tsenh@farmingdale.edu>
2024-02-22 21:30:23 -0500

    StockDiviner-3 Deletes LunarCalculator classes from branch as branches should be separated by work! I hate myself.

--- src/main/java/org/farmingdale/stockdiviner/model/lunar/LunarCalculatorAPI.java
+++
@@ -1,8 +0,0 @@
-package org.farmingdale.stockdiviner.model.lunar;
-
-import java.io.IOException;
-import java.time.LocalDate;
-
-public interface LunarCalculatorAPI {
-    LunarPhase getLunarPhase(LocalDate date) throws IOException;
-}
