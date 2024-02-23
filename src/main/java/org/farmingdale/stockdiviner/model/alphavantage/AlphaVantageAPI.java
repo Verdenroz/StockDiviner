d93b866ce6870b247e93eba988fc388d2bcaa198
harvey <tsenh@farmingdale.edu>
2024-02-22 21:35:54 -0500

    StockDiviner-16 Deletes classes from LunarCalculator and AlphaVantage to keep branches separate

--- src/main/java/org/farmingdale/stockdiviner/model/alphavantage/AlphaVantageAPI.java
+++
@@ -1,7 +0,0 @@
-package org.farmingdale.stockdiviner.model.alphavantage;
-
-import java.io.IOException;
-
-public interface AlphaVantageAPI {
-    MonthlyStockData getMonthlyTimeSeries(String symbol) throws IOException;
-}
