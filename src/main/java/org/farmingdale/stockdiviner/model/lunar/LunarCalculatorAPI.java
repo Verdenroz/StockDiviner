c27adff0eb85d732d6c588f841e1a16695f6d8d9
harvey <tsenh@farmingdale.edu>
2024-03-07 12:20:40 -0500

    StockDiviner-4 Adds documentation for LunarCalculatorAPI

--- src/main/java/org/farmingdale/stockdiviner/model/lunar/LunarCalculatorAPI.java
+++ src/main/java/org/farmingdale/stockdiviner/model/lunar/LunarCalculatorAPI.java
@@ -3,6 +3,12 @@ package org.farmingdale.stockdiviner.model.lunar;
 import java.io.IOException;
 import java.time.LocalDate;
 
+/**
+ * Interface for the LunarCalculatorAPI
+ */
 public interface LunarCalculatorAPI {
+    /**
+     * Returns the lunar phase for a given date
+     */
     LunarPhase getLunarPhase(LocalDate date) throws IOException;
 }
