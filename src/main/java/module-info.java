03a278feef81a465461c587fc636667a261ccb9d
harvey <tsenh@farmingdale.edu>
2024-03-06 19:07:16 -0500

    StockDiviner-2 Fix dependencies for firebase

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,11 +3,20 @@ module org.farmingdale.stockdiviner {
     requires javafx.fxml;
     requires okhttp3;
     requires com.google.gson;
+    requires com.google.auth;
+    requires com.google.auth.oauth2;
+    requires google.cloud.firestore;
+    requires google.cloud.core;
+    requires firebase.admin;
+    requires com.google.api.apicommon;
 
 
     opens org.farmingdale.stockdiviner to javafx.fxml;
     exports org.farmingdale.stockdiviner;
+    exports org.farmingdale.stockdiviner.model;
     exports org.farmingdale.stockdiviner.model.alphavantage;
     exports org.farmingdale.stockdiviner.model.lunar;
     exports org.farmingdale.stockdiviner.model.animals;
+    exports org.farmingdale.stockdiviner.model.firebase;
+
 }
