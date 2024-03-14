6cff05b8fdd811d4f6898c93dcfd198e7399be60
Kevin Paiz Ramos <paizkevin27@gmail.com>
2024-03-14 17:42:07 -0400

    StockDiviner-20 StockSearch add A method so when a key is pressed a autocompletion bar would appear, search bar uses hard code stocks

--- src/main/java/org/farmingdale/stockdiviner/SearchController.java
+++ src/main/java/org/farmingdale/stockdiviner/SearchController.java
@@ -2,8 +2,17 @@ package org.farmingdale.stockdiviner;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import org.controlsfx.control.textfield.AutoCompletionBinding;
+import org.controlsfx.control.textfield.TextFields;
+
+import java.net.URL;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.ResourceBundle;
+import java.util.Set;
 
 public class SearchController {
 
@@ -21,12 +30,28 @@ public class SearchController {
 
     @FXML
     private Button zodiacSignsButton;
+    private AutoCompletionBinding<String> autoCompletionBinding;
+    private String[] possibleSuggestions = {"Apple APPL",
+                                            "Microsof MSFT",
+            "Google GOOGL",
+            "Amazon AMZN",
+            "Facebook FB",
+            "Tesla TSLA",
+            "Netflix NFLX",
+            "Alphabet GOOGL",
+            "Alphabet GOOG",
+            "Nvidia NVDA",
+            "Paypal PYPL",
+            "Adobe ADBE","Shopify SHOP","Salesforce CRM","Zoom ZM","Microsoft MSFT","Apple AAPL","Amazon AMZN","Facebook FB","Tesla TSLA","Netflix NFLX","Alphabet GOOGL","Alphabet GOOG","Nvidia NVDA","Paypal PYPL","Adobe ADBE","Shopify SHOP","Salesforce CRM","Zoom ZM","Microsoft MSFT","Apple AAPL","Amazon AMZN","Facebook FB","Tesla TSLA","Netflix NFLX","Alphabet GOOGL","Alphabet GOOG","Nvidia NVDA","Paypal PYPL","Adobe ADBE","Shopify SHOP","Salesforce CRM","Zoom ZM","Microsoft MSFT","Apple AAPL","Amazon AMZN","Facebook FB","Tesla TSLA","Netflix NFLX","Alphabet GOOGL","Alphabet GOOG","Nvidia NVDA","Paypal PYPL","Adobe ADBE","Shopify SHOP","Salesforce CRM","Zoom ZM","Microsoft MSFT","Apple AAPL","Amazon AMZN","Facebook FB","Tesla TSLA","Netflix NFLX","Alphabet GOOGL","Alphabet GOOG","Nvidia NVDA","Paypal PYPL","Adobe ADBE","Shopify SHOP","Salesforce CRM","Zoom ZM","Microsoft MSFT","Apple AAPL","Amazon AMZN","Facebook FB","Tesla TSLA","Netflix NFLX","Alphabet GOOGL","Alphabet GOOG","Nvidia NVDA","Paypal PYPL","Adobe ADBE","Shopify SHOP","Salesforce CRM","Zoom ZM","Microsoft MSFT","Apple AAPL","Amazon AMZN","Facebook FB","Tesla TSLA","Netflix NFLX","Alphabet GOOGL","Alphabet GOOG","Nvidia NVDA","Paypal PYPL","Adobe ADBE","Shopify SHOP","Salesforce CRM","Zoom ZM","Microsoft MSFT","Apple AAPL","Amazon AMZN","Facebook FB","Tesla TSLA","Netflix NFLX","Alphabet GOOGL","Alphabet GOOG","Nvidia NVDA","Paypal PYPL","Adobe ADBE","Shopify SHOP","Salesforce CRM","Zoom ZM","Microsoft MSFT",};
 
+    private Set<String> possibleSuggestionsSet = new HashSet<>(Arrays.asList(possibleSuggestions));
 
-    public void printMessage(ActionEvent event) {
-        System.out.println("Hello World");
-       // TextFields.bindAutoCompletion(searchBarTextField, "AAPL", "GOOGL", "MSFT", "TSLA", "AMZN", "FB", "NFLX", "NVDA", "INTC", "AMD");
+    public void printMessage() {
+
+        TextFields.bindAutoCompletion(searchBarTextField, possibleSuggestionsSet).setMaxWidth(searchBarTextField.getWidth());
     }
+
+
 //cannot use initialize method in this class because it is not a subclass of Application
 
 
