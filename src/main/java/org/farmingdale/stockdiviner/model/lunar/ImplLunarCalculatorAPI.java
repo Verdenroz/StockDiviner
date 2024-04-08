368ad9cb325583b86a50ee61d466353832728a0e
harvey <harveytseng2@gmail.com>
2024-04-08 16:40:29 -0400

    StockDiviner-23 Refactor LunarCalculator to return a map of <LocalDate, LunarPhase> instead

--- src/main/java/org/farmingdale/stockdiviner/model/lunar/ImplLunarCalculatorAPI.java
+++ src/main/java/org/farmingdale/stockdiviner/model/lunar/ImplLunarCalculatorAPI.java
@@ -11,8 +11,8 @@ import java.io.IOException;
 import java.lang.reflect.Type;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class ImplLunarCalculatorAPI implements LunarCalculatorAPI {
     private static volatile LunarCalculatorAPI instance;
@@ -21,7 +21,15 @@ public class ImplLunarCalculatorAPI implements LunarCalculatorAPI {
     private final OkHttpClient client;
 
     private ImplLunarCalculatorAPI() {
-        this.client = new OkHttpClient();
+        this.client = new OkHttpClient.Builder()
+                .addInterceptor(chain -> {
+                    Request request = chain.request().newBuilder()
+                            .build();
+
+                    System.out.println(request.url());
+                    return chain.proceed(request);
+                })
+                .build();
     }
 
     /**
@@ -39,7 +47,7 @@ public class ImplLunarCalculatorAPI implements LunarCalculatorAPI {
     }
 
     @Override
-    public LunarPhase getLunarPhase(LocalDate date) throws IOException {
+    public Map<LocalDate, LunarPhase> getLunarPhase(LocalDate date) throws IOException {
         String formattedDate = date.format(formatter);
         HttpUrl base = HttpUrl.parse(LUNAR_CALCULATOR_URL);
         HttpUrl url = base.newBuilder()
@@ -48,7 +56,7 @@ public class ImplLunarCalculatorAPI implements LunarCalculatorAPI {
                 .addPathSegment("phases")
                 .addPathSegment("date")
                 .addQueryParameter("date", formattedDate)
-                .addQueryParameter("nump", "1")
+                .addQueryParameter("nump", "99")
                 .build();
         Request request = new Request.Builder()
                 .url(url)
@@ -63,11 +71,24 @@ public class ImplLunarCalculatorAPI implements LunarCalculatorAPI {
 
             JsonObject jsonObject = gson.fromJson(response.body().charStream(), JsonObject.class);
             JsonArray phasedataArray = jsonObject.getAsJsonArray("phasedata");
-            Type listType = new TypeToken<ArrayList<PhaseData>>(){}.getType();
+            Type listType = new TypeToken<ArrayList<PhaseData>>() {
+            }.getType();
             List<PhaseData> phasedataList = gson.fromJson(phasedataArray, listType);
 
-            PhaseData phaseData = phasedataList.get(0);
-            return LunarPhase.valueOf(phaseData.getPhase().toUpperCase().replace(" ", "_"));
+            Map<LocalDate, LunarPhase> lunarPhases = new HashMap<>();
+
+            for (PhaseData phaseData : phasedataList) {
+                LocalDate phaseDate = LocalDate.of(phaseData.getYear(), phaseData.getMonth(), phaseData.getDay());
+
+                LunarPhase lunarPhase = LunarPhase.valueOf(phaseData.getPhase().toUpperCase().replace(" ", "_"));
+
+                lunarPhases.put(phaseDate, lunarPhase);
+            }
+            // Sort the lunarPhases map by keys (dates) in descending order
+            return lunarPhases.entrySet().stream()
+                    .sorted(Map.Entry.<LocalDate, LunarPhase>comparingByKey().reversed())
+                    .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+
         } catch (JsonSyntaxException e) {
             throw new IOException("Error parsing JSON", e);
         }
