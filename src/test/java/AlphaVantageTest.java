85337960aa23d23208245f33ad6bf9b05ce2071c
harvey <harveytseng2@gmail.com>
2024-04-11 22:05:48 -0400

    StockDiviner-30 Remove unnecessary setup

--- src/test/java/AlphaVantageTest.java
+++ src/test/java/AlphaVantageTest.java
@@ -15,22 +15,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class AlphaVantageTest {
 
-    @BeforeEach
-    public void setup() {
-        // Load properties and set system property
-        Properties properties = new Properties();
-        try (InputStream input = getClass().getClassLoader().getResourceAsStream("config.properties")) {
-            if (input == null) {
-                throw new IOException("Unable to find config.properties");
-            }
-            properties.load(input);
-            String apiKey = properties.getProperty("alphaVantage.apiKey");
-            System.setProperty("alphaVantage.apiKey", apiKey);
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-    }
-
     @Test
     void getMonthlyTimeSeries() {
         // Arrange
