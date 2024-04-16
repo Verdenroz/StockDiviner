3b145794669a0ecee519f5c87a8ccb0a233ffab8
harvey <harveytseng2@gmail.com>
2024-04-16 14:19:39 -0400

    StockDiviner-31 Create bulk endpoint to get FullQuoteData for multiple symbols

--- src/main/java/org/farmingdale/stockdiviner/model/financialmodeling/FinancialModelingAPI.java
+++ src/main/java/org/farmingdale/stockdiviner/model/financialmodeling/FinancialModelingAPI.java
@@ -10,10 +10,17 @@ import java.util.List;
 public interface FinancialModelingAPI {
     /**
      * Get the full quote data for a given stock symbol
-     * @param symbol the symbol of the stock
+     * @param symbols the symbol of the stock
      * @return FullQuoteData object containing all the data for the stock
      */
-    FullQuoteData getFullQuoteData(String symbol) throws IOException;
+    FullQuoteData getFullQuoteData(String symbols) throws IOException;
+
+    /**
+     * Get full quote data for multiple stocks
+     * @param symbols the symbols of the stocks
+     * @return List of FullQuoteData objects containing the data for the stocks
+     */
+    List<FullQuoteData> getBulkQuotes(String... symbols) throws IOException;
 
     /**
      * Search for a stock by name or symbol
